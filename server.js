2ce5b1d9c09371a9090879a71d561627611e8e43
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-09-16 02:10:36 +0400

    - change port

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/log', async (req, res) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = 80;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
